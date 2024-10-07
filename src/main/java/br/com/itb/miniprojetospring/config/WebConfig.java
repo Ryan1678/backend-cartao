682d4915fe42234575fbaa7870e743be66cd9f6e
Ryan1678 <ryanamorim623@gmail.com>
2024-10-07 10:45:23 -0300

    rss

---
+++ src/main/java/br/com/itb/miniprojetospring/config/WebConfig.java
@@ -0,0 +1,19 @@
+package br.com.itb.miniprojetospring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Habilita CORS para todas as rotas
+                .allowedOrigins("http://localhost:5173") // Permitir requisições do frontend (React)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permitir esses métodos
+                .allowedHeaders("*") // Permitir todos os cabeçalhos
+                .allowCredentials(true); // Permitir envio de cookies ou credenciais
+    }
+}
